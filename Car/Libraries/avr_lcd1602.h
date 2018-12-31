6640d449ce4f30aaa2a956b3bd55acc754421f60
JosephPenaQuino <joseph.pena@utec.edu.pe>
2018-12-30 20:54:10 -0500

    Adding LCD Library

---
+++ Car/Libraries/avr_lcd1602.h
@@ -0,0 +1,28 @@
+/*
+* avr_lcd1602.h
+* 	
+* http://www.electronicwings.com
+*/
+
+#ifndef AVR_LCD1602_H_
+#define AVR_LCD1602_H_
+
+// #define  F_CPU 1000000UL
+// #include <avr/io.h>
+// #include <util/delay.h>
+#define LCD_DPRT PORTD
+#define LCD_DDDR DDRD	
+#define LCD_RS 2
+#define LCD_EN 3
+
+void LCD_Command(unsigned char cmnd);
+void LCD_Char(unsigned char data);
+void LCD_Init();
+void LCD_String_xy(unsigned char, unsigned char, char *);
+void LCD_String(char *str);
+void LCD_Clear();
+
+
+
+
+#endif 
